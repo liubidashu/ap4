c8b7a4a77bd6e1b7b814e8dc369dffd0e47e8ea3
liubidashu <114441846+liubidashu@users.noreply.github.com>
2023-06-23 12:55:38 +0300

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    $('.menu').click(function () {
+        $('.menu_popup').toggleClass('none');
+    });
+$(".wk").click(function() {
+    $(".wk").addClass("none");
+    $(".sk").removeClass("none");
+});
+$(".wk2").click(function() {
+    $(".wk2").addClass("none");
+    $(".sk2").removeClass("none");
+});
+$(".wk3").click(function() {
+    $(".wk3").addClass("none");
+    $(".sk3").removeClass("none");
+});
+$(".wt").click(function() {
+    $(".wt").addClass("none");
+    $(".st").removeClass("none");
+});
+$(".wt2").click(function() {
+    $(".wt2").addClass("none");
+    $(".st2").removeClass("none");
+});
+$(".wt3").click(function() {
+    $(".wt3").addClass("none");
+    $(".st3").removeClass("none");
+});
+$(".bottom-gotovo").click(function() {
+    $(".window").toggleClass("none");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
